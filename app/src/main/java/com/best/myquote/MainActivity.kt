d36d37a8aab74f52e4250b9e641a2ae8283fa6c0
abdul rohman <abdulrohmanunida@gmail.com>
2023-09-29 11:08:15 +0700

    parsing data on method

--- app/src/main/java/com/best/myquote/MainActivity.kt
+++ app/src/main/java/com/best/myquote/MainActivity.kt
@@ -2,11 +2,15 @@ package com.best.myquote
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.best.myquote.databinding.ActivityMainBinding
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
+import org.json.JSONObject
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,15 +38,38 @@ class MainActivity : AppCompatActivity() {
                 headers: Array<Header>,
                 responseBody: ByteArray
             ) {
-                    TODO("Not yet implemented")
+                binding.progressBar.visibility = View.INVISIBLE
+
+                val result = String(responseBody)
+                Log.d(TAG, result)
+                try {
+                    val responseObject = JSONObject(result)
+
+                    val quote = responseObject.getString("en")
+                    val author = responseObject.getString("author")
+
+                    binding.tvQuote.text = quote
+                    binding.tvAuthor.text = author
+                } catch (e: Exception) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    e.printStackTrace()
+                }
             }
             override fun onFailure(
                 statusCode: Int,
-                headers: Array<out Header>?,
-                responseBody: ByteArray?,
-                error: Throwable?
+                headers: Array<Header>,
+                responseBody: ByteArray,
+                error: Throwable
             ) {
-                TODO("Not yet implemented")
+                binding.progressBar.visibility = View.INVISIBLE
+
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
             }
         })
     }
